b70c7602ba05af71498f8d2d297cb7743cea150c
Vladimir Lanevich <38554304+vnlanevich@users.noreply.github.com>
2018-08-29 12:09:58 +0300

    plane

--- plane/Complex.java
+++ plane/Complex.java
@@ -4,7 +4,6 @@ import static java.lang.Math.*;
 
 class Complex extends Point {
 
-
     Complex(double re, double im) {
         x = re;
         y = im;
@@ -15,12 +14,10 @@ class Complex extends Point {
         this.y = y;
     }
 
-
     public double getModules(Complex complex1) {
 
         return sqrt(pow((this.x + complex1.x), 2.0)
                 + pow((this.y + complex1.y), 2.0));
-
     }
 
 }
